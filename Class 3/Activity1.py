cdf28897d6f1ba128d3b1fb4ee89d2740f0f707f
Kiaan-18 <kapse.kiaan@gmail.com>
2025-03-16 10:12:36 +0100

    Add files via upload

---
+++ Class 3/Activity1.py
@@ -0,0 +1,9 @@
+#datatypes in python
+a=5
+print("datatype of a is",type (a))
+b="Kiaan"
+print("datatype of b is",type(b))
+c=1.9
+print("datatype of c is ",type(c))
+d=True
+print("datatype of c is ",type(d))
